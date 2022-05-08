af0e624eac28fd764fa5c27485e9a73925df9c77
kimjh3 <imjhk03@gmail.com>
2022-05-09 01:10:44 +0900

    Routing

--- Viper/TripListRouter.swift
+++ Viper/TripListRouter.swift
@@ -28,45 +28,14 @@
 
 import Foundation
 import SwiftUI
-import Combine
 
-class TripListPresenter: ObservableObject {
-    private let interactor: TripListInteractor
-    private let router = TripListRouter()
-    @Published var trips: [Trip] = []
-    private var cancellables = Set<AnyCancellable>()
-    
-    init(interactor: TripListInteractor) {
-        self.interactor = interactor
-        
-        interactor.model.$trips
-            .assign(to: \.trips, on: self)
-            .store(in: &cancellables)
-    }
-    
-    func makeAddNewButton() -> some View {
-        Button(action: addNewTrip) {
-            Image(systemName: "plus")
-        }
-    }
-    
-    func addNewTrip() {
-        interactor.addNewTrip()
-    }
-    
-    func deleteTrip(_ index: IndexSet) {
-        interactor.deleteTrip(index)
-    }
-    
-    func linkBuilder<Content: View>(
-        for trip: Trip,
-        @ViewBuilder content: () -> Content
-    ) -> some View {
-        NavigationLink(
-            destination: router.makeDetailView(
-                for: trip,
-                model: interactor.model)) {
-                    content()
-        }
+class TripListRouter {
+    func makeDetailView(for trip: Trip, model: DataModel) -> some View {
+        let presenter = TripDetailPresenter(interactor:
+                                                TripDetailInteractor(
+                                                    trip: trip,
+                                                    model: model,
+                                                    mapInfoProvider: RealMapDataProvider()))
+        return TripDetailView(presenter: presenter)
     }
 }
